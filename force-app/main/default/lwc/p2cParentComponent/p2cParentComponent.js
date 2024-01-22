5a941cf55b4d87464b5d9c210ef2664382c138ae
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-22 19:24:12 +0100

    Parent to Child Communication using non-primitive data types

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,26 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+    carouselDetails = [
+        {
+            src: "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header:"First Card",
+            description: "First card description.",
+            altText: "First card accessible description.",
+            href: "https://www.example.com"
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header : "Second Card",
+            description : "Second card description.",
+            altText : "Second card accessible description.",
+            href : "https://www.example.com"
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header : "Third Card",
+            altText : "Third card accessible description.",
+            href : "https://www.example.com"
+        }
+    ]
+}
