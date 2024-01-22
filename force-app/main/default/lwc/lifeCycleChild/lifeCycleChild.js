49623eeb79f9953a6eecde9bf060858a1117ecf0
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-22 15:43:05 +0100

    Add consrtuctor connectedCallback and renderedCallback lifecycle hooks

--- force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
+++ force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class LifeCycleChild extends LightningElement {}
+export default class LifeCycleChild extends LightningElement {
+    constructor() {
+        super();
+        console.log('child constructor called!')
+    }
+    connectedCallback() {
+        console.log('child connected callback called!')
+    }
+    renderedCallback() {
+        console.log('child rendered callback called!')
+    }
+}
