9e1b8c279d6f2dfebbc6cd891e8a3a5b1cbd0aa0
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-22 21:25:47 +0100

    Child to parent communication using simple events

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class C2pModalComponent extends LightningElement {}
+export default class C2pModalComponent extends LightningElement {
+    
+    closeHandler () {
+        const myCloseEvent = new CustomEvent('close')
+        this.dispatchEvent(myCloseEvent)
+    }
+}
--- force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class C2pParentComponent extends LightningElement {}
+export default class C2pParentComponent extends LightningElement {
+    showModal = false
+
+    clickhandler() {
+        this.showModal = true
+    }
+    closeHandler() {
+        this.showModal = false
+    }
+}
