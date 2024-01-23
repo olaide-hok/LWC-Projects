c4e753f78608e8b0bfd1c845155c34fa9f7bdd07
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-23 17:04:58 +0100

    setter method

--- force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
+++ force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class SetterDemoChild extends LightningElement {}
+export default class SetterDemoChild extends LightningElement {
+    userDetail
+    @api
+    get detail() {
+        return this.userDetail
+    }
+    set detail(data) {
+        let newAge = data.age * 2
+        this.userDetail = {...data, age: newAge, 'location': 'Maldives'}
+    }
+
+}
