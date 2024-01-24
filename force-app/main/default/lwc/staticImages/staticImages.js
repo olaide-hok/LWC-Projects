a39287a17554bd11b6eb80bf11990bcfd535d99f
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-24 15:18:14 +0100

    images from static resources

--- force-app/main/default/lwc/staticImages/staticImages.js
+++ force-app/main/default/lwc/staticImages/staticImages.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
-
-export default class StaticImages extends LightningElement {}
+import USER_IMAGE from '@salesforce/resourceUrl/user_image'
+import USER_WALKING_IMG from '@salesforce/resourceUrl/user_walking'
+export default class StaticImages extends LightningElement {
+    userImage = USER_IMAGE
+    userWalking= USER_WALKING_IMG
+}
