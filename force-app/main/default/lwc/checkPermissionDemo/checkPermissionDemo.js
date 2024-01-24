8b7023e2686d3b766b73af02c1d306387d0acb92
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-24 17:50:47 +0100

    Check Permissions

--- force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
+++ force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import hasViewAllData from '@salesforce/userPermission/ViewAllData'
+import myCustomPermission from '@salesforce/customPermission/show_details'
 
-export default class CheckPermissionDemo extends LightningElement {}
+export default class CheckPermissionDemo extends LightningElement {
+    
+    get hasViewAllDataAvailable() {
+        return hasViewAllData
+    }
+
+    get hasCustomPermission() {
+        return myCustomPermission
+    }
+}
