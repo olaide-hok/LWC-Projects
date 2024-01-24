2516c1a9b69ca22fdf815c45c2ae8ab0bf8d85ce
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-24 16:54:34 +0100

    Internationalization in LWC

--- force-app/main/default/lwc/internationalization/internationalization.js
+++ force-app/main/default/lwc/internationalization/internationalization.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import LOCALE from '@salesforce/i18n/local'
+import CURRENCY from '@salesforce/i18n/currency'
+import DIR from '@salesforce/i18n/dir'
 
-export default class Internationalization extends LightningElement {}
+export default class Internationalization extends LightningElement {
+    dir = DIR
+    number = 5439340.98
+    formattedNumber = new Intl.NumberFormat(LOCALE, {
+        style: 'currency',
+        currency: CURRENCY,
+        currencyDisplay: 'symbol'
+    }).format(this.number)
+}
