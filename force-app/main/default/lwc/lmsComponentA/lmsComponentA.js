3f59d5d6ee36142d3f7ddc03fa3c4dc13bf602d5
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-01-24 07:29:04 +0100

    Setup lmsComponentA to send out message

--- force-app/main/default/lwc/lmsComponentA/lmsComponentA.js
+++ force-app/main/default/lwc/lmsComponentA/lmsComponentA.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+import { MessageContext, publish } from 'lightning/messageService';
 
-export default class LmsComponentA extends LightningElement {}
+export default class LmsComponentA extends LightningElement {
+    inputValue
+
+    @wire(MessageContext)
+    context
+
+    inputHandler(event) {
+        this.inputValue = event.target.value;
+    }
+
+    //publish(messageContext, messageChannel, message)
+    publishMessage() {
+        const message = {
+            lmsData: {
+                value: this.inputValue
+            }
+        }
+        publish(this.context, SAMPLEMC, message)
+    }
+}
